738b3456a176153ae1efb85bcfddb72e7eb3f50f
Larry Reid <lcreid@jadesystems.ca>
2018-05-03 16:40:06 -0700

    Rails 5.2. Woo hoo!

--- Gemfile
+++ Gemfile
@@ -1,7 +1,9 @@
 source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.1.0'
+gem 'rails', '~> 5.2.0'
+
+gem 'bootsnap'
 
 # Use sqlite3 as the database for Active Record
 # include it in all environments or the build won't work.
--- config/application.rb
+++ config/application.rb
@@ -11,10 +11,11 @@ module Pbs
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
     config.action_mailer.default_url_options = { host: 'cleanroutines.com' }
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
