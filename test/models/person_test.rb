930d7259c44b598a1559fe11701b8110a7d168aa
Larry Reid <lcreid@jadesystems.ca>
2018-05-15 18:14:18 -0700

    Change of DB. Change of order.

--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -1,10 +1,10 @@
-=begin
-This Source Code Form is subject to the terms of the Mozilla Public
-License, v. 2.0. If a copy of the MPL was not distributed with this
-file, You can obtain one at http://mozilla.org/MPL/2.0/.
-Copyright (c) Jade Systems Inc. 2013, 2014
-=end
-require 'test_helper'
+# frozen_string_literal: true
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# Copyright (c) Jade Systems Inc. 2013, 2014
+require "test_helper"
 
 class PersonTest < ActiveSupport::TestCase
   test "person one has one User link" do
@@ -12,7 +12,7 @@ class PersonTest < ActiveSupport::TestCase
     assert_equal 1, person.links.size
     assert_equal "User Two", person.links.first.person_b.short_name
   end
-  
+
   test "person two has one Patient link" do
     person = people(:user_five)
     assert_equal 1, person.links.size
@@ -28,13 +28,13 @@ class PersonTest < ActiveSupport::TestCase
     assert_equal "User Three", person.links.joins(:person_b).where("user_id is not null").first.person_b.short_name
   end
 
-test "person one has one User" do
+  test "person one has one User" do
     person = people(:user_one)
     assert_equal 1, person.people.size
     assert_equal "User Two", person.people.first.short_name
     assert_equal "Two Google", person.users.first.name
   end
-  
+
   test "person two has one Patient" do
     person = people(:user_five)
     assert_equal 1, person.people.size
@@ -49,162 +49,162 @@ test "person one has one User" do
     assert_equal "Patient Two", person.patients.first.short_name
     assert_equal "Three Yahoo", person.users.first.name
   end
-  
+
   test "Don't link people already linked" do
     p10 = people(:p10_secondary)
     p20 = people(:p20_secondary)
-    
+
     assert_no_difference "p10.people.count" do
       p10.linkup(p20)
     end
   end
-  
+
   test "find dates for all routines for a person" do
     person = people(:person_marty)
     assert_equal 2, person.routines.size
     assert_equal 4, person.routine_dates.size
   end
-  
+
   test "hash for completed routines detailed overview" do
-    Time.zone = 'Samoa'
+    Time.zone = "Samoa"
     correct_hash = {
-      DateTime.parse('2014-01-15T00:00:00') => [
+      DateTime.parse("2014-01-15T00:00:00") => [
         routines(:routine_index_one) => [completed_routines(:routine_index_one_one)]
       ],
-      DateTime.parse('2014-01-16T00:00:00') => [
+      DateTime.parse("2014-01-16T00:00:00") => [
         routines(:routine_index_two) => [completed_routines(:routine_index_two_one)]
       ],
-      DateTime.parse('2014-01-17T00:00:00') => [
+      DateTime.parse("2014-01-17T00:00:00") => [
         routines(:routine_index_one) => [completed_routines(:routine_index_one_one)]
       ],
-      DateTime.parse('2014-01-18T00:00:00') => [
+      DateTime.parse("2014-01-18T00:00:00") => [
         routines(:routine_index_one) => [completed_routines(:routine_index_one_one)]
       ]
     }
     correct_completed_routines_column_layout = [
-      Time.zone.parse('2014-01-01T00:00:00'), [
-        [Time.zone.parse('2014-01-30T00:00:00'), 1],
-        [Time.zone.parse('2014-01-31T00:00:00'), 1]
+      Time.zone.parse("2014-01-01T00:00:00"), [
+        [Time.zone.parse("2014-01-30T00:00:00"), 1],
+        [Time.zone.parse("2014-01-31T00:00:00"), 1]
       ],
-      Time.zone.parse('2014-02-01T00:00:00'), [
-        [Time.zone.parse('2014-02-01T00:00:00'), 2],
-        [Time.zone.parse('2014-02-02T00:00:00'), 2]
+      Time.zone.parse("2014-02-01T00:00:00"), [
+        [Time.zone.parse("2014-02-01T00:00:00"), 2],
+        [Time.zone.parse("2014-02-02T00:00:00"), 2]
       ]
     ]
-    
+
     person = people(:person_marty)
     hash = person.completed_routines_column_layout
     assert_equal correct_completed_routines_column_layout, hash
   end
-  
+
   test "routines layout" do
-    Time.zone = 'Samoa'
+    Time.zone = "Samoa"
     correct_hash = {
-      routines(:routine_index_one)=> {
-        Time.zone.local(2014, 01, 30)=> [
+      routines(:routine_index_one) => {
+        Time.zone.local(2014, 1, 30) => [
           completed_routines(:routine_index_one_one)
         ],
-        Time.zone.local(2014, 02, 01)=> [
+        Time.zone.local(2014, 2, 1) => [
           completed_routines(:routine_index_one_two)
         ],
-        Time.zone.local(2014, 02, 02)=> [
+        Time.zone.local(2014, 2, 2) => [
           completed_routines(:routine_index_one_three),
           completed_routines(:routine_index_one_four)
         ]
       },
-      routines(:routine_index_two)=> {
-        Time.zone.local(2014, 01, 31)=> [
+      routines(:routine_index_two) => {
+        Time.zone.local(2014, 1, 31) => [
           completed_routines(:routine_index_two_one)
         ],
-        Time.zone.local(2014, 02, 01)=> [
+        Time.zone.local(2014, 2, 1) => [
           completed_routines(:routine_index_two_two)
         ]
       }
     }
-#    puts "I am a MiniTest #{self.kind_of?(MiniTest::Unit::TestCase)}"
+    #    puts "I am a MiniTest #{self.kind_of?(MiniTest::Unit::TestCase)}"
     person = people(:person_marty)
-#    correct_hash.each { |k, v| puts k.id; v.each { |k, v| puts "  #{k} #{k.class}"; v.each { |x| puts "    #{x.id}"}}}
-#    person.routines_layout.each { |k, v| puts k.id; v.each { |k, v| puts "  #{k} #{k.class}"; v.each { |x| puts "    #{x.id}"}}}
+    #    correct_hash.each { |k, v| puts k.id; v.each { |k, v| puts "  #{k} #{k.class}"; v.each { |x| puts "    #{x.id}"}}}
+    #    person.routines_layout.each { |k, v| puts k.id; v.each { |k, v| puts "  #{k} #{k.class}"; v.each { |x| puts "    #{x.id}"}}}
     assert_equal correct_hash, person.routines_layout
   end
-  
+
   test "whole layout" do
-    Time.zone = 'Samoa'
+    Time.zone = "Samoa"
     correct_hash = {
       routines(:routine_index_one) => {
         expectations(:ri0101) => {
-          Time.zone.local(2014, 01, 30)=> [
+          Time.zone.local(2014, 1, 30) => [
             completed_expectations(:ri0101)
           ],
-          Time.zone.local(2014, 02, 01)=> [
+          Time.zone.local(2014, 2, 1) => [
             completed_expectations(:ri0103)
           ],
-          Time.zone.local(2014, 02, 02)=> [
+          Time.zone.local(2014, 2, 2) => [
             completed_expectations(:ri0106)
           ]
         },
         expectations(:ri0102) => {
-          Time.zone.local(2014, 02, 01)=> [
+          Time.zone.local(2014, 2, 1) => [
             completed_expectations(:ri0104)
           ],
-          Time.zone.local(2014, 02, 02)=> [
+          Time.zone.local(2014, 2, 2) => [
             completed_expectations(:ri0107),
             completed_expectations(:ri0109)
           ]
         },
         expectations(:ri0103) => {
-          Time.zone.local(2014, 01, 30)=> [
+          Time.zone.local(2014, 1, 30) => [
             completed_expectations(:ri0102)
           ],
-          Time.zone.local(2014, 02, 02)=> [
-            completed_expectations(:ri0108),
-            completed_expectations(:ri010a)
+          Time.zone.local(2014, 2, 2) => [
+            completed_expectations(:ri010a),
+            completed_expectations(:ri0108)
           ]
         },
         expectations(:ri0104) => {
-          Time.zone.local(2014, 02, 01)=> [
+          Time.zone.local(2014, 2, 1) => [
             completed_expectations(:ri0105)
           ],
-          Time.zone.local(2014, 02, 02)=> [
+          Time.zone.local(2014, 2, 2) => [
             completed_expectations(:ri010b)
           ]
         }
       },
       routines(:routine_index_two) => {
         expectations(:ri0201) => {
-          Time.zone.local(2014, 01, 31)=> [
+          Time.zone.local(2014, 1, 31) => [
             completed_expectations(:ri0201)
           ],
-          Time.zone.local(2014, 02, 01)=> [
+          Time.zone.local(2014, 2, 1) => [
             completed_expectations(:ri0203)
           ]
         },
         expectations(:ri0202) => {
-          Time.zone.local(2014, 01, 31)=> [
+          Time.zone.local(2014, 1, 31) => [
             completed_expectations(:ri0202)
           ],
-          Time.zone.local(2014, 02, 01)=> [
+          Time.zone.local(2014, 2, 1) => [
             completed_expectations(:ri0204)
           ]
         }
       }
     }
-  
+
     person = people(:person_marty)
     full_layout = person.full_layout
     assert_equal correct_hash[routines(:routine_index_two)], full_layout[routines(:routine_index_two)]
-    assert_equal correct_hash[routines(:routine_index_one)][expectations(:ri0101)], 
+    assert_equal correct_hash[routines(:routine_index_one)][expectations(:ri0101)],
       full_layout[routines(:routine_index_one)][expectations(:ri0101)]
-    assert_equal correct_hash[routines(:routine_index_one)][expectations(:ri0102)], 
+    assert_equal correct_hash[routines(:routine_index_one)][expectations(:ri0102)],
       full_layout[routines(:routine_index_one)][expectations(:ri0102)]
-    assert_equal correct_hash[routines(:routine_index_one)][expectations(:ri0103)], 
+    assert_equal correct_hash[routines(:routine_index_one)][expectations(:ri0103)],
       full_layout[routines(:routine_index_one)][expectations(:ri0103)]
-    assert_equal correct_hash[routines(:routine_index_one)][expectations(:ri0104)], 
+    assert_equal correct_hash[routines(:routine_index_one)][expectations(:ri0104)],
       full_layout[routines(:routine_index_one)][expectations(:ri0104)]
     assert_equal correct_hash[routines(:routine_index_one)], full_layout[routines(:routine_index_one)]
     assert_equal correct_hash, full_layout
   end
-  
+
   test "Connections through secondary IDs" do
     # Test that a person connected only to a user's secondary id shows up in the person's
     # user association.
@@ -213,15 +213,15 @@ test "person one has one User" do
     assert p30.users.include?(p10)
   end
 
-#  test "link_to" do
-#    marie = users(:user_marie)
-#    max = people(:patient_max)
-#    stella = people(:person_stella)
-#    not_connected = people(:user_five)
-#    
-#    assert_equal 4, marie.people.count
-#    assert_not_nil max.link_to(marie)
-#    assert_not_nil stella.link_to(marie)
-#    assert_nil marie.primary_identity.link_to(not_connected)
-#  end
+  #  test "link_to" do
+  #    marie = users(:user_marie)
+  #    max = people(:patient_max)
+  #    stella = people(:person_stella)
+  #    not_connected = people(:user_five)
+  #
+  #    assert_equal 4, marie.people.count
+  #    assert_not_nil max.link_to(marie)
+  #    assert_not_nil stella.link_to(marie)
+  #    assert_nil marie.primary_identity.link_to(not_connected)
+  #  end
 end
