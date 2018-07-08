053a79d875378d7c7107d8a69d776bcde48f5d1f
Larry Reid <lcreid@jadesystems.ca>
2018-07-07 20:55:51 -0700

    Format create routine.

--- test/system/team_building_test.rb
+++ test/system/team_building_test.rb
@@ -18,14 +18,11 @@ class TeamBuildingTest < ApplicationSystemTestCase
   end
 
   test "save backwards from routine" do
-    skip
-
     get_logged_in(:user_marie)
 
     person = people(:patient_matt)
-    visit(home_user_path(@user))
-    click_link("Matt-Patient")
-    assert_equal person_path(person), current_path
+    visit(edit_person_path(person))
+    assert_current_path edit_person_path(person)
     click_link("New Routine")
     assert_equal new_routine_path, current_path
     fill_in "Name", with: "Capy add"
@@ -34,7 +31,7 @@ class TeamBuildingTest < ApplicationSystemTestCase
     # puts body
     # puts "Console: #{page.driver.console_messages}"
     # puts "Errors: #{page.driver.error_messages}"
-    assert has_selector?("input[id*='description']")
+    assert_selector("input[id*='description']")
     find("input[id$=_description]").set("Exp 1")
     assert_difference "Expectation.all.count" do
       assert_difference "person.routines.reload.count" do
@@ -50,13 +47,13 @@ class TeamBuildingTest < ApplicationSystemTestCase
     click_link(matt.short_name)
     click_link("Edit Subject")
     check("Stella")
-    assert_difference "matt.people.reload.count" do
+    assert_difference "matt.people.count" do
       click_button("Save")
     end
     assert_equal person_path(matt), current_path
     click_link("Edit Subject")
     uncheck("Stella")
-    assert_difference "matt.people.reload.count", -1 do
+    assert_difference "matt.people.count", -1 do
       click_button("Save")
     end
   end
